91a58e6a2a37d5cd278ac01268ee0ae9fe685f56
Abdullahi Sani Salisu <abdullahisanisalisu5@gmail.com>
2026-08-17 07:21:15 +0100

    Create AnimatedContainer.jsx

---
+++ src/components/AnimatedContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+export default function AnimatedContainer({ children, className = '' }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 15 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -15 }}
+      transition={{ duration: 0.3, ease: 'easeInOut' }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+}
